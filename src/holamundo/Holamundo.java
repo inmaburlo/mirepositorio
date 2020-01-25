920879972a055f85436bfbfa75248fcd3d8f3ce6
Inmaculada Burlo De Rojas <inmaburlo@gmail.com>
2020-01-25 12:34:14 +0100

    copiado el contendio de la carpeta HolaMundo (proyecto de Netbeans) en nuestra carpeta mirepositorio, para usar un control de versiones sobre ese proyecto que tenemos en nuestro ordenador.

---
+++ src/holamundo/Holamundo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Muki
+ */
+public class Holamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO de application logic here
+        System.out.println("Holi mundo");
+       Scanner sc= new Scanner (System.in);
+        int n;
+       System.out.println("Introduzca un numero entero: ");
+       n = sc.nextInt();
+       
+    }
+    
+}
+
